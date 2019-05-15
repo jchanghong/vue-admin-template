0e09032ec84426c3cd984fe2349f2b664eb79b16
jchanghong <3200601@qq.com>
2019-05-15 21:59:52 +0800

    init

--- src/permission.js
+++ src/permission.js
@@ -10,10 +10,60 @@ NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login'] // no redirect whitelist
 
+async function extracted(to, next) {
+  if (to.path === '/login') {
+    // if is logged in, redirect to the home page
+    next({ path: '/' })
+    NProgress.done()
+  } else {
+    // determine whether the user has obtained his permission roles through getInfo
+    const hasRoles = store.getters.roles && store.getters.roles.length > 0
+    if (hasRoles) {
+      next()
+    } else {
+      try {
+        // get user info
+        // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+        const { roles } = await store.dispatch('user/getInfo')
+
+        // generate accessible routes map based on roles
+        const accessRoutes = await store.dispatch('permission/generateRoutes',
+          roles)
+
+        // dynamically add accessible routes
+        router.addRoutes(accessRoutes)
+
+        // hack method to ensure that addRoutes is complete
+        // set the replace: true, so the navigation will not leave a history record
+        next({ ...to, replace: true })
+      } catch (error) {
+        // remove token and go to login page to re-login
+        await store.dispatch('user/resetToken')
+        Message.error(error || 'Has Error')
+        next(`/login?redirect=${to.path}`)
+        NProgress.done()
+      }
+    }
+  }
+}
+function getQueryParam(url){
+  var url = decodeURI(url); //获取url中"?"符后的字串
+  var theRequest =new Object();
+  if (url.indexOf("?") != -1) {
+    var str = url.substr(url.indexOf('?')+1);
+    console.info(str)
+   let strs = str.split("&");
+    for(var i = 0; i <strs.length; i++) {
+      theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+    }
+  }
+  console.info(theRequest)
+  return theRequest;
+}
 router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
-
+  getQueryParam(to.fullPath)
   // set page title
   document.title = getPageTitle(to.meta.title)
 
@@ -21,42 +71,14 @@ router.beforeEach(async(to, from, next) => {
   const hasToken = getToken()
 
   if (hasToken) {
-    if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
-    } else {
-      // determine whether the user has obtained his permission roles through getInfo
-      const hasRoles = store.getters.roles && store.getters.roles.length > 0
-      if (hasRoles) {
-        next()
-      } else {
-        try {
-          // get user info
-          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const { roles } = await store.dispatch('user/getInfo')
-
-          // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-
-          // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
-
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
-          next({ ...to, replace: true })
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
-      }
-    }
+    await extracted(to, next)
   } else {
     /* has no token*/
-
+    // get user info
+    // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+    console.info(to.fullPath)
+     await store.dispatch('user/login',{username:'admin',password:'pass'})
+   await extracted(to,next)
     if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
       next()
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -27,15 +27,18 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
+    console.info(userInfo)
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
+        console.info('login ok')
         resolve()
       }).catch(error => {
         reject(error)
+        console.info('login error'+error.toString())
       })
     })
   },
